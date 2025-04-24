1d600153b7a9fd22f3d2286463b9d315d132ba3a
Lucas Paes <lucas@Mac-Pro-de-Lucas.local>
2025-04-23 21:52:44 -0300

    feat: refactor pagination handling by creating PaginationProductList component

---
+++ src/shared/ui/organisms/pagination/pagination.tsx
@@ -0,0 +1,46 @@
+'use client'
+
+import {
+  Pagination,
+  PaginationContent,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
+
+interface PaginationDemoProps {
+  currentPage: number;
+  handleNextPage: () => void;
+  handlePreviuesPage: () => void;
+}
+
+export const PaginationProductList = ({
+  currentPage,
+  handleNextPage,
+  handlePreviuesPage,
+}: PaginationDemoProps) => {
+  return (
+    <Pagination className="mt-3">
+      <PaginationContent>
+        <PaginationItem
+          className="bg-purple-medium text-white rounded-md"
+          onClick={handlePreviuesPage}
+        >
+          <PaginationPrevious href="#" />
+        </PaginationItem>
+
+        <PaginationItem className="bg-purple-medium text-white rounded-md">
+          <PaginationLink>{currentPage}</PaginationLink>
+        </PaginationItem>
+
+        <PaginationItem
+          className="bg-purple-medium text-white rounded-md"
+          onClick={handleNextPage}
+        >
+          <PaginationNext href="#" />
+        </PaginationItem>
+      </PaginationContent>
+    </Pagination>
+  );
+}
--- src/shared/ui/organisms/product-item-list/product-item-list.tsx
+++ src/shared/ui/organisms/product-item-list/product-item-list.tsx
@@ -1,39 +1,29 @@
-'use client'
+"use client";
 
 import { useShoppingList } from "@/app/providers/shopping-list-provider";
-import { ItemProps, ProductItem } from "../../molecules/product-item/product-item";
+import {
+  ItemProps,
+  ProductItem,
+} from "../../molecules/product-item/product-item";
 import { EmptyList } from "../../molecules/empty-list/empty-list";
 import { Spinner } from "../../atoms/loading/loading";
 
-import {
-  Pagination,
-  PaginationContent,
-  PaginationEllipsis,
-  PaginationItem,
-  PaginationLink,
-  PaginationNext,
-  PaginationPrevious,
-} from "@/components/ui/pagination"
-
+import { PaginationProductList } from "../pagination/pagination";
 
 const sortProducts = (products: ItemProps[]): ItemProps[] => {
   return [...products].sort((a, b) => Number(a.checked) - Number(b.checked));
 };
 
 export const ProductItemList = () => {
-
-  const { products, isLoading, handleNextPage, handlePreviuesPage } = useShoppingList()
-
-  console.log(products)
+  const { products, isLoading, handleNextPage, handlePreviuesPage } =
+    useShoppingList();
 
   if (isLoading) {
-    return (
-      <Spinner size='large' className="text-white" />
-    )
+    return <Spinner size="large" className="text-white" />;
   }
 
   if (products.data.length === 0) {
-    return (<EmptyList />)
+    return <EmptyList />;
   }
 
   return (
@@ -41,37 +31,12 @@ export const ProductItemList = () => {
       {sortProducts(products.data).map((item) => (
         <ProductItem key={`${item.id}-${item.name}`} {...item} />
       ))}
-      <PaginationDemo currentPage={products.currentPage} handleNextPage={handleNextPage} handlePreviuesPage={handlePreviuesPage} {...products} />
+      <PaginationProductList
+        currentPage={products.currentPage}
+        handleNextPage={handleNextPage}
+        handlePreviuesPage={handlePreviuesPage}
+      />
     </div>
   );
 };
 
-interface PaginationDemoProps {
-  currentPage: number;
-  handleNextPage: () => void;
-  handlePreviuesPage: () => void;
-}
-
-
-export function PaginationDemo({ currentPage, handleNextPage, handlePreviuesPage }: PaginationDemoProps) {
-
-  return (
-    <Pagination className="mt-3">
-      <PaginationContent>
-        <PaginationItem className="bg-purple-medium text-white rounded-md" onClick={handlePreviuesPage}>
-          <PaginationPrevious href="#" />
-        </PaginationItem>
-
-        <PaginationItem className="bg-purple-medium text-white rounded-md">
-          <PaginationLink >
-            {currentPage}
-          </PaginationLink>
-        </PaginationItem>
-
-        <PaginationItem className="bg-purple-medium text-white rounded-md" onClick={handleNextPage}>
-          <PaginationNext href="#" />
-        </PaginationItem>
-      </PaginationContent>
-    </Pagination>
-  )
-}
